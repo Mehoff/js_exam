b8256bca8ea28ae6b296cf05c1c3a0ac132a8930
Михов Илья Сергеевич <Mihokw39>
2021-07-01 11:56:10 +0300

    more changes - nothing works :(

--- enemy.js
+++ enemy.js
@@ -15,7 +15,7 @@ class Bullet{
         this.x = x;
         this.y = y;
         this.angle = angle;
-        isAboutToDieFlag = false;
+        this.isAboutToDieFlag = false;
     }
 
     do(){
@@ -73,9 +73,45 @@ class Entity {
     position
     speed
     radius
+    fillColor
     isReadyToShoot
     shootDelayMs
+    controller
     bullets
+
+    constructor(controller){
+        this.controller = controller;
+    }
+
+    draw(){    
+        
+        this.controller.ctx.beginPath();
+        this.controller.ctx.arc(this.position.x, this.position.y, this.radius, 2 * Math.PI, false);
+        this.controller.ctx.fillStyle = this.fillColor;
+        this.controller.ctx.fill();
+        this.controller.ctx.lineWidth = 3;
+        this.controller.ctx.strokeStyle = '#000000';
+        this.controller.ctx.stroke();
+
+        console.log(this.controller.ctx.fillStyle)
+    }
+
+    processBullets(){
+        {
+            for(let bullet of this.bullets){
+                bullet.do();
+            }
+            for(let i = this.bullets.length - 1; i >= 0; i--){
+                if(this.bullets[i].isAboutToDie === true){
+                    const index = this.bullets.indexOf(this.bullets[i]);
+                    if(index > -1){
+                        this.bullets.splice(index, 1)
+                        console.log('bullet dead')
+                    }
+                }
+            }
+        }
+    }
 
 }
 
@@ -84,32 +120,31 @@ class Player extends Entity{
         position,
         speed,
         radius,
-        bulletTemplate
+        fillColor,
+        movementController
     ){
-        super();
+        super(movementController);
         this.position = position;
         this.speed = speed;
         this.radius = radius;
-        
-        this.bulletTemplate = bulletTemplate
+        this.fillColor = fillColor;
+
         this.bullets = [];
-
         this.isReadyToShoot = true;
-        this.shootDelayMs = 100;
-
-        console.log();
+        this.shootDelayMs = 100; 
     }
 
-    shoot(context, targetX, targetY){
-        const deltaX = this.position.x - targetX;
-        const deltaY = this.position.y - targetY;
+    shoot(){
+        const deltaX = this.position.x - this.controller.mouse.position.x;
+        const deltaY = this.position.y - this.controller.mouse.position.y;
         const rotation = (Math.atan2(deltaY, deltaX)) * 180 / Math.PI
         const radians = (2 * Math.PI / 360) * (rotation - 180) * -1;
         
 
-        let bullet = new PlayerBullet(context,
-            this.position.x - (this.bulletTemplate.width / 2),
-            this.position.y - (this.bulletTemplate.height / 2),
+        let bullet = new PlayerBullet(this.controller.ctx,
+            // fix
+            this.position.x - (10 / 2),
+            this.position.y - (10 / 2),
             radians
         );
         
@@ -119,11 +154,130 @@ class Player extends Entity{
         //this.bullets.push(bullet);
     }
 
-    
+    act(){
+        this.controller.act(this);
+    }
+
+    processBullets(){
+        this.controller.processBullets(this.bullets)
+    }
+
+
 }
 
 class Enemy extends Entity{
     constructor(){
+
+    }
+}
+
+
+
+class MovementController{
+
+    // isMoving
+    // isRunning
+    // up
+    // down
+    // left
+    // right
+    // degrees
+    // entity
+
+    // mouse
+
+    constructor(ctx){
+        this.ctx = ctx;
+
+        this.isMoving = false;
+        this.isRunning = false;
+        this.up = false;
+        this.down = false;
+        this.left = false;
+        this.right = false;
+        this.degress = 0;
+        this.entity = undefined
+        
+        this.mouse = {
+            left: false,
+            right: false,
+            position: {x: 0, y: 0}
+        }            
+    }
+
+
+    setEntity(entity){
+        this.entity = entity
+    }
+
+    setMousePosition(event, canvas){
+        const rect = canvas.getBoundingClientRect();
+        this.mouse.position.x = event.clientX - rect.left;
+        this.mouse.position.y = event.clientY - rect.right;
+    }
+    setMousePosition(canvas, pageX, pageY){
+        const rect = canvas.getBoundingClientRect();
+        this.mouse.position.x = pageX - rect.left;
+        this.mouse.position.y = pageY - rect.right;
+    }
+
+    handleKey(event){
+        const type = event.type;
+        const key = event.code;
+        const action = keyCodes[key];
+    
+        if(action){
+            if(action.startsWith('move')){
+                this.isMoving = (type === 'keydown');
+                switch(action){
+                            case 'move_left': {
+                                this.left = this.isMoving
+                            } break;
+                            case 'move_right': {
+                                this.right = this.isMoving
+                            } break;
+                            case 'move_up': {
+                                this.up = this.isMoving
+                            } break;
+                            case 'move_down': {
+                                this.down = this.isMoving
+                            } break;
+                        default: console.log(`action ${action} is not specified`)
+                }
+            }
+        }
+    }
+
+
+    processBullets(bullets){
+        {
+            for(let bullet of bullets){
+                bullet.do();
+            }
+            for(let i = bullets.length - 1; i >= 0; i--){
+                if(bullets[i].isAboutToDie === true){
+                    const index = bullets.indexOf(bullets[i]);
+                    if(index > -1){
+                        bullets.splice(index, 1); console.log('bullet dead');
+                    }
+                }
+            }
+        }
+    }
+
+    act(entity){
+        if(this.up){
+            entity.position.y -= this.isRunning === true ? ((entity.speed * 1.5) * secondsPassed) : (entity.speed * secondsPassed) 
+        }
+        if(this.down){
+            entity.position.y += this.isRunning === true ? ((entity.speed * 1.5) * secondsPassed) : (entity.speed * secondsPassed) 
+        }
+        if(this.left){
+            entity.position.x -= this.isRunning === true ? ((entity.speed * 1.5) * secondsPassed) : (entity.speed * secondsPassed);
+        }
+        if(this.right){
+            entity.position.x += this.isRunning === true ? ((entity.speed * 1.5) * secondsPassed) : (entity.speed * secondsPassed);
+        }
 
     }
 }
--- index.js
+++ index.js
@@ -14,111 +14,113 @@ const keyCodes = {
     'ShiftLeft': 'run'
 }
 
-const mainPlayer = new Player(100,100,30, typeof(PlayerBullet))
-
-let PLAYER = {
-    position: {
-        x : 100,
-        y : 100
-    },
-    speed: 50,
-    runSpeed: 100,
-    radius: 30,
-    strokeWidth: 3,
-    color: '#ffb6c1',
-    stroke: 'black',
+const controller = new MovementController(ctx);
+const mainPlayer = new Player(100, 100, 30, 'red', controller)
 
 
-    // Use Later
-    isReadyToShoot: true,
-    shootDelayMs: 100,
-    bulletWidth: 5,
-    bulletHeight: 10,
-    bulletSpeed: 10,
+// let PLAYER = {
+//     position: {
+//         x : 100,
+//         y : 100
+//     },
+//     speed: 50,
+//     runSpeed: 100,
+//     radius: 30,
+//     strokeWidth: 3,
+//     color: '#ffb6c1',
+//     stroke: 'black',
 
-    bullets: [],
 
-    shoot(canvasX, canvasY){
-        // SetTimeOut on shootDelay
+//     // Use Later
+//     isReadyToShoot: true,
+//     shootDelayMs: 100,
+//     bulletWidth: 5,
+//     bulletHeight: 10,
+//     bulletSpeed: 10,
+
+//     bullets: [],
+
+//     shoot(canvasX, canvasY){
+//         // SetTimeOut on shootDelay
         
-        let deltaX = PLAYER.position.x - canvasX;
-        let deltaY = PLAYER.position.y - canvasY;
+//         let deltaX = PLAYER.position.x - canvasX;
+//         let deltaY = PLAYER.position.y - canvasY;
 
-        let rotation = (Math.atan2(deltaY, deltaX)) * 180 / Math.PI
-        let radians = (2 * Math.PI / 360) * (rotation - 180) * -1;
+//         let rotation = (Math.atan2(deltaY, deltaX)) * 180 / Math.PI
+//         let radians = (2 * Math.PI / 360) * (rotation - 180) * -1;
         
-        let bullet = new Bullet(ctx, this.position.x - (this.bulletWidth / 2), this.position.y - (this.bulletHeight / 2), this.bulletWidth, this.bulletheight, radians, this.bulletSpeed)
-        this.bullets.push(bullet);
-    },
+//         let bullet = new Bullet(ctx, this.position.x - (this.bulletWidth / 2), this.position.y - (this.bulletHeight / 2), this.bulletWidth, this.bulletheight, radians, this.bulletSpeed)
+//         this.bullets.push(bullet);
+//     },
 
-    processBullets(){
-        for(let bullet of this.bullets){
-            bullet.do();
-        }
-        for(let i = this.bullets.length - 1; i >= 0; i--){
-            if(this.bullets[i].isAboutToDie === true){
-                const index = this.bullets.indexOf(this.bullets[i]);
-                if(index > -1){
-                    this.bullets.splice(index, 1)
-                    console.log('bullet dead')
-                }
-            }
-        }
-    }
-}
+//     processBullets(){
+//         for(let bullet of this.bullets){
+//             bullet.do();
+//         }
+//         for(let i = this.bullets.length - 1; i >= 0; i--){
+//             if(this.bullets[i].isAboutToDie === true){
+//                 const index = this.bullets.indexOf(this.bullets[i]);
+//                 if(index > -1){
+//                     this.bullets.splice(index, 1)
+//                     console.log('bullet dead')
+//                 }
+//             }
+//         }
+//     }
+// }
 
-let CONTROLLER = {
+// let CONTROLLER = {
 
-    isMoving: false,
-    isRunning: false,
-    up: false,
-    down: false,
-    left: false,
-    right: false,
+//     isMoving: false,
+//     isRunning: false,
+//     up: false,
+//     down: false,
+//     left: false,
+//     right: false,
 
-    mouse: {
-        left : false,
-        right : false,
-        position: {
-            x : 0,
-            y : 0
-        }
-    },
+//     mouse: {
+//         left : false,
+//         right : false,
+//         position: {
+//             x : 0,
+//             y : 0
+//         }
+//     },
 
-    degrees : 0,
+//     degrees : 0,
 
-    act(){
-        if(this.up){
-            PLAYER.position.y -= this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed) 
-        }
-        if(this.down){
-            PLAYER.position.y += this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed) 
-        }
-        if(this.left){
-            PLAYER.position.x -= this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed);
-        }
-        if(this.right){
-            PLAYER.position.x += this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed);
-        }
+//     act(){
+//         if(this.up){
+//             PLAYER.position.y -= this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed) 
+//         }
+//         if(this.down){
+//             PLAYER.position.y += this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed) 
+//         }
+//         if(this.left){
+//             PLAYER.position.x -= this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed);
+//         }
+//         if(this.right){
+//             PLAYER.position.x += this.isRunning === true ? (PLAYER.runSpeed * secondsPassed) : (PLAYER.speed * secondsPassed);
+//         }
 
-        PLAYER.processBullets()
-    },
-    setMousePosition(x, y){
-        this.mouse.position.x = x;
-        this.mouse.position.y = y
-    }
-}
+//         PLAYER.processBullets()
+//     },
+//     setMousePosition(x, y){
+//         this.mouse.position.x = x;
+//         this.mouse.position.y = y
+//     }
+// }
 
-let ENEMY = {
-    position: {
-        x : 0,
-        y : 0
-    },
-    speed: 50,
-    radius: 50,
-    color: 'red',
-    stroke: 'black'
-}
+// let ENEMY = {
+//     position: {
+//         x : 0,
+//         y : 0
+//     },
+//     speed: 50,
+//     radius: 50,
+//     color: 'red',
+//     stroke: 'black'
+// }
 
 const CENTER_X = ctx.width / 2;
 const CENTER_Y = ctx.height / 2;
@@ -129,15 +131,8 @@ function drawBackground(){
 }
 
 function drawPlayer(){
-    ctx.beginPath();
-    
-    ctx.arc(PLAYER.position.x, PLAYER.position.y, PLAYER.radius, 2 * Math.PI, false);
-    ctx.fillStyle = PLAYER.color;
-    ctx.fill();
-    
-    ctx.lineWidth = PLAYER.strokeWidth;
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
+    mainPlayer.draw();
+    mainPlayer.act();
 }
 
 function gameLoop(timestamp){
@@ -148,7 +143,7 @@ function gameLoop(timestamp){
 
     clearCanvas();
     drawCall();
-    CONTROLLER.act();
+    //mainPlayer.controller.act();
     drawFPS();
 
     // SPOT FOR DEBUGGING
@@ -161,21 +156,25 @@ function gameLoop(timestamp){
 
 function drawFPS(){
     FPS = Math.round(1 / secondsPassed)
-    ctx.font = '18px Verdana';
+    ctx.font = '22px Verdana';
     ctx.fillStyle = 'white'
     ctx.fillText(`FPS: ${FPS}`, 20, 20)
 }
 
 function drawCall(){
+
+    clearCanvas();
     drawBackground();
     drawPlayer();
 
-    ctx.beginPath();
-    ctx.moveTo(PLAYER.position.x, PLAYER.position.y);
 
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = 'red'
-    ctx.stroke();
+
+    // ctx.beginPath();
+    // ctx.moveTo(mainPlayer.position.x, mainPlayer.position.y);
+
+    // ctx.lineWidth = 10;
+    // ctx.strokeStyle = 'red'
+    // ctx.stroke();
 }
 
 function clearCanvas(){
@@ -186,62 +185,56 @@ function clearCanvas(){
 function onMousePress(event){
     
     event.preventDefault();
-    
-    if(event.target.id === 'main'){
-        switch(event.button){
-        case 0:
-
-            let rect = canvas.getBoundingClientRect();
-            let x = event.clientX - rect.left;
-            let y = event.clientY - rect.top;
-            
-            PLAYER.shoot(x, y);
-            break;
-        }
-    }
+    //mainPlayer.movementController.setMousePosition(canvas, event)
+    mainPlayer.shoot()
 }
 
-function HandleKey(event){
+// function HandleKey(event){
 
-    const type = event.type;
-    const key = event.code;
-    const action = keyCodes[key];
+//     const type = event.type;
+//     const key = event.code;
+//     const action = keyCodes[key];
 
-    if(action){
+//     if(action){
 
-//        CONTROLLER.isRunning = (action === 'ShiftLeft');
+// //        CONTROLLER.isRunning = (action === 'ShiftLeft');
 
-        console.log(CONTROLLER.isRunning)
+//         console.log(CONTROLLER.isRunning)
 
-        if(action.startsWith('move')){
-            CONTROLLER.isMoving = (type === 'keydown');
-            switch(action){
-                        case 'move_left': {
-                            CONTROLLER.left = CONTROLLER.isMoving
-                        } break;
-                        case 'move_right': {
-                            CONTROLLER.right = CONTROLLER.isMoving
-                        } break;
-                        case 'move_up': {
-                            CONTROLLER.up = CONTROLLER.isMoving
-                        } break;
-                        case 'move_down': {
-                            CONTROLLER.down = CONTROLLER.isMoving
-                        } break;
-                        default: console.log(`action ${action} is not specified`)
-                }
-        }
-    }
-}
+//         if(action.startsWith('move')){
+//             CONTROLLER.isMoving = (type === 'keydown');
+//             switch(action){
+//                         case 'move_left': {
+//                             CONTROLLER.left = CONTROLLER.isMoving
+//                         } break;
+//                         case 'move_right': {
+//                             CONTROLLER.right = CONTROLLER.isMoving
+//                         } break;
+//                         case 'move_up': {
+//                             CONTROLLER.up = CONTROLLER.isMoving
+//                         } break;
+//                         case 'move_down': {
+//                             CONTROLLER.down = CONTROLLER.isMoving
+//                         } break;
+//                         default: console.log(`action ${action} is not specified`)
+//                 }
+//         }
+//     }
+// }
 
 function HandleMouseMove(event){
+    // if(event.target.id === 'main'){
+    //     CONTROLLER.setMousePosition(event.pageX, event.pageY)
+    // }
+
     if(event.target.id === 'main'){
-        CONTROLLER.setMousePosition(event.pageX, event.pageY)
+        // Test if pageX and pageY is correct
+        mainPlayer.controller.setMousePosition(canvas, event.pageX, event.pageY);
     }
 }
 
-document.addEventListener('keyup', HandleKey)
-document.addEventListener('keydown', HandleKey)
+document.addEventListener('keyup', mainPlayer.controller.handleKey)
+document.addEventListener('keydown', mainPlayer.controller.handleKey)
 document.addEventListener('mousedown', (event) => onMousePress(event))
 document.addEventListener('mousemove', HandleMouseMove);
 
