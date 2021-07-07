b97dcf7ef047a261ce207684a68e271c8861848b
Михов Илья Сергеевич <Mihokw39>
2021-07-07 13:55:12 +0300

    player shoot delay

--- player.js
+++ player.js
@@ -15,25 +15,34 @@ class Player extends Entity{
 
         this.bullets = [];
         this.isReadyToShoot = true;
-        this.shootDelayMs = 100;
+        this.shootDelayMs = 300;
         this.maxhp = maxhp;
         this.hp = this.maxhp;
+        this.canShoot = true;
     }
 
     shoot(){
 
-        let deltaX = this.position.x - this.controller.mouse.position.x;
-        let deltaY = this.position.y - this.controller.mouse.position.y;
+        if(this.canShoot == true){
+            let deltaX = this.position.x - this.controller.mouse.position.x;
+            let deltaY = this.position.y - this.controller.mouse.position.y;
 
-        let rotation = (Math.atan2(deltaY, deltaX)) * 180 / Math.PI
-        let radians = (2 * Math.PI / 360) * (rotation - 180) * -1;
+            let rotation = (Math.atan2(deltaY, deltaX)) * 180 / Math.PI
+            let radians = (2 * Math.PI / 360) * (rotation - 180) * -1;
 
-        let bullet = new PlayerBullet(this.controller.ctx,
-            {x: this.position.x - (10/ 2), y: this.position.y - (10 / 2)},
-            radians, this
-        );
-        
-        this.bullets.push(bullet);
+            let bullet = new PlayerBullet(this.controller.ctx,
+                {x: this.position.x - (10/ 2), y: this.position.y - (10 / 2)},
+                radians, this
+            );
+            
+            this.bullets.push(bullet);
+
+            this.canShoot = false;
+            
+            setTimeout(() => {
+                this.canShoot = true;
+            }, this.shootDelayMs)
+        }
     }
 
     act(){
